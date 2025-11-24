9d90fa45cb4c92c94150e0aa8701d9c2ab1cdda8
Skimlk <vanhoutencocoapowder@gmail.com>
2025-11-24 16:14:56 -0500

    NETSEC-3: Never user unix path

--- doc2lnk/path_operations.py
+++ doc2lnk/path_operations.py
@@ -5,7 +5,7 @@ def convert_to_os_specific_path(directory):
     try:
         return wslPath.to_windows(directory)
     except Exception:
-        return wslPath.to_posix(directory)
+        return directory
 
 def get_directory(file_path):
     directory = os.path.dirname(file_path)
