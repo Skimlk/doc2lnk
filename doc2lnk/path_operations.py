0da658a5a78015b8c2cff305f8070fc1b643d086
Skimlk <vanhoutencocoapowder@gmail.com>
2025-11-24 16:29:55 -0500

    NETSEC-3: Fix crossplatform path issue

--- doc2lnk/path_operations.py
+++ doc2lnk/path_operations.py
@@ -1,10 +1,14 @@
 import wslPath
+import platform
 import os
 
 def convert_to_os_specific_path(directory):
     try:
         return wslPath.to_windows(directory)
     except Exception:
+        if platform.system() == 'Linux':
+            return wslPath.to_posix(directory)
+
         return directory
 
 def get_directory(file_path):
